e1f03f58dee2c70b04b6d368d6a840de0863bfad
zheng-yongping <zheng-yongping@hotmail.com>
2019-09-24 22:28:22 +0800

    add: method_details source template

---
+++ lib/templates/default/method_details/html/setup.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+def detect_language(filename)
+  puts filename
+  case File.extname(filename).downcase
+  when '.c' then 'C'
+  when '.py' then 'Python'
+  when '.rb' then 'Ruby'
+  when '.c++', '.cpp', '.cxx' then 'Cpp'
+  end
+end
